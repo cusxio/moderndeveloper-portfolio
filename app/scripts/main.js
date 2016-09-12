edf5f48a1ebaf46183cf2d67f11949741068430e
Jonathan Chan <cusxio@gmail.com>
2016-09-12 23:23:24 +0800

    new color scheme, new design, nav shadow

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,9 +7,21 @@
         const navButtons = document.querySelectorAll('.nav__links a[href*="#"]');
         [].forEach.call(navButtons, function (el) {
             el.addEventListener('click', function () {
-                document.querySelector('.nav__links .active').classList.remove('active');
-                el.parentNode.classList.add('active');
+                if (document.querySelector('.nav__links .nav-active')) {
+                    document.querySelector('.nav__links .nav-active').classList.remove('nav-active');
+                    el.classList.add('nav-active');
+                } else {
+                    el.classList.add('nav-active');
+                }
             });
+        });
+
+        window.addEventListener('scroll', function () {
+            if (document.body.scrollTop > 0) {
+                document.querySelector('nav').classList.add('body-is-scrolled');
+            } else {
+                document.querySelector('nav').classList.remove('body-is-scrolled');
+            }
         });
     });
 })(document);
