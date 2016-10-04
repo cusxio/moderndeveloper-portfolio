cf5dd2bbe7b8775b630b55804721d272a3005ba8
Jonathan Chan <cusxio@gmail.com>
2016-10-04 08:46:37 +0800

    Update 2016-10-04T00:46:31.833Z

--- gulpfile.babel.js/tasks/surge.js
+++
@@ -1,8 +0,0 @@
-import surge from 'gulp-surge';
-
-export default function ghPages() {
-    return surge({
-        project: './public',
-        domain: 'https://md-portfolio.surge.sh',
-    });
-}
